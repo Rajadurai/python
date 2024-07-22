535e9c45cf9672f62b8b680fe67f78d4e9eda38a
RAJADURAI <rajadurai1981@gmail.com>
2024-07-22 17:00:33 +0530

    Create reverseWords.py
    everse the words in the given string

---
+++ reverseWords.py
@@ -0,0 +1,14 @@
+# reverse the words in the given string
+
+string = "geeksd quiz practice code"
+rev_string = string.split()[::-1]
+print(rev_string)
+
+#l=[]
+#for i in rev_string:
+    #l.append(i)
+
+#print(" ".join(l))
+
+join = " ".join(rev_string)
+print(join)
